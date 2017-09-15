ca71576a18f6ab2ebb72532e103a5a3e408d953c
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-15 21:02:59 +0700

    server

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const { MongoClient } = require('mongodb');
 const express = require('express');
 
+let wordsCollection;
+
 const app = express();
 app.set('view engine', 'ejs');
 app.set('views', './views');
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.render('home'));
+app.get('/', (req, res) => {
+    wordsCollection.find().toArray()
+    .then(result => res.render('home', { arrWords: result }))
+    .catch(err => res.send(err.message));
+});
 
 const url = 'mongodb://localhost:27017/shop';
 
 MongoClient.connect(url)
 .then(db => {
     app.listen(3000, () => console.log('Server started!'));
-    const words = db.collection('words');
-    return words.find().toArray();
+    wordsCollection = db.collection('words');
 })
-.then(result => console.log(result))
+.then(result => arrWords = result)
 .catch(err => console.log(err.message));
