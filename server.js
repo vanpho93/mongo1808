ec604b0a176fad829d71c56236fd38932b27a72e
Van Pho Nguyen <vanpho02@gmail.com>
2017-09-18 21:20:45 +0700

    xong

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const parser = require('body-parser').urlencoded({ extended: false });
+
 const Word = require('./Word');
 mongoose.Promise = global.Promise;
 
@@ -9,8 +11,38 @@ app.set('views', './views');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    Word.find({})
+    Word.find()
     .then(result => res.render('home', { arrWords: result }));
+});
+
+app.get('/remove/:id', (req, res) => {
+    const { id } = req.params;
+    Word.findByIdAndRemove(id)
+    .then(() => res.redirect('/'))
+    .catch(err => res.send(err.message));
+});
+
+app.get('/edit/:id', (req, res) => {
+    const { id } = req.params;
+    Word.findById(id)
+    .then(result => res.render('edit', { result }))
+    .catch(err => res.send(err.message));
+});
+
+app.post('/edit/:id', parser, (req, res) => {
+    const { id } = req.params;
+    const { en, vn } = req.body;
+    Word.findByIdAndUpdate(id, { en, vn })
+    .then(() => res.redirect('/'))
+    .catch(err => res.send(err.message));
+});
+
+app.post('/add', parser, (req, res) => {
+    const { en, vn } = req.body;
+    const word = new Word({ en, vn });
+    word.save()
+    .then(() => res.redirect('/'))
+    .catch(err => res.send(err.message));
 });
 
 const uri = 'mongodb://localhost/shop';
